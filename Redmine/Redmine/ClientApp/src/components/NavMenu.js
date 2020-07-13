11b2e12e6ec257ac9ee431907b65d9e18b1f9a7f
Mayank Jain <mayankbitrmech@gmail.com>
2020-07-13 13:02:41 +0530

    added title bar

--- Redmine/Redmine/ClientApp/src/components/NavMenu.js
+++ Redmine/Redmine/ClientApp/src/components/NavMenu.js
@@ -31,7 +31,13 @@ export class NavMenu extends Component {
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
+                  <NavLink tag={Link} className="text-dark" to="/">Search</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink tag={Link} className="text-dark" to="/">Search input</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink tag={Link} className="text-dark" to="/">Search projects</NavLink>
                 </NavItem>
               </ul>
             </Collapse>
---
+++ Redmine/Redmine/ClientApp/src/components/TitleBar.js
@@ -0,0 +1,53 @@
+﻿import React, { Component } from 'react';
+import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import './NavMenu.css';
+
+export class TitleBar extends Component {
+    static displayName = TitleBar.name;
+
+    constructor(props) {
+        super(props);
+
+        this.toggleTitleBar = this.toggleTitleBar.bind(this);
+        this.state = {
+            collapsed: true
+        };
+    }
+
+    toggleTitleBar() {
+        this.setState({
+            collapsed: !this.state.collapsed
+        });
+    }
+
+    render() {
+        return (
+            <header>
+                <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
+                    <Container>
+                        <ul className="navbar-nav flex-grow">
+                            <NavItem>
+                                <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink tag={Link} className="text-dark" to="/">Project</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink tag={Link} className="text-dark" to="/">Help</NavLink>
+                            </NavItem>
+                        </ul>
+                        <NavbarToggler onClick={this.toggleTitleBar} className="mr-2" />
+                        <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
+                            <ul className="navbar-nav flex-grow">
+                                <NavItem>
+                                    <NavLink tag={Link} className="text-dark" to="/">Sign In</NavLink>
+                                </NavItem>
+                            </ul>
+                        </Collapse>
+                    </Container>
+                </Navbar>
+            </header>
+        );
+    }
+}
