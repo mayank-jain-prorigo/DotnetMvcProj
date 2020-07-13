b2f4c5827bbf6659b51d65fd322fba5219064107
Mayank Jain <mayankbitrmech@gmail.com>
2020-07-13 13:08:16 +0530

    added data model class

---
+++ Redmine/Redmine/Model/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Redmine.Model
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int EmpId { get; set; }
+        public DateTime RegisteredOn { get; set; }
+        public DateTime LastActive { get; set; }
+        public string PhoneNo { get; set; }
+        public IEnumerable<Issue> Issues { get; set; }
+        public IEnumerable<Project> Projects { get; set; }
+    }
+}
